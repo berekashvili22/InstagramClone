d84b12a3e1fda2b8ac5c7a5f302f02bad485d6d4
Tornike Berekashvili <tornike.berekashvili@pm.me>
2020-11-14 23:20:15 +0400

    Initial commit

---
+++ app/Profile.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Profile extends Model
+{
+    protected $guarded = [];
+
+    public function profileImage()
+    {
+        $imagePath = ($this->image) ?  $this->image : 'profile/d3CpZg0kPydcpd2vAArSI2uZjcipNJ6r2Wzjvfun.png';
+        return '/storage/' . $imagePath;
+    }
+
+    public function followers()
+    {
+        return $this->belongsToMany(User::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+    
+
+}

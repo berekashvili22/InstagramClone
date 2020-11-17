3c4afaffa414a980110720531d0361e3b1d9bd35
Tornike Berekashvili <tornike.berekashvili@pm.me>
2020-11-17 22:57:13 +0400

    created store comment route

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,9 @@ Route::post('follow/{user}', 'FollowsController@store');
 Route::get('/', 'PostController@index')->name('posts.index');
 Route::get('/p/create', 'PostController@create')->name('posts.create');
 Route::post('/p', 'PostController@store')->name('posts.store');
+
+Route::post('/p/comment', 'CommentsController@store')->name('comments.create');
+
 Route::get('/p/{post}', 'PostController@show')->name('posts.show');
 
 Route::get('/profile/{user}', 'ProfilesController@index')->name('profile.show');
